4d049b10c36324f034751bfe5725788ae07b38d1
Suhana <suhana0921.be20@chitkara.edu.in>
2022-12-13 22:37:56 +0530

    first commit

---
+++ insertionSort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void display(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+   int n;
+   cout << "Enter Size of array";
+   cin >> n;
+   int arr[n];    
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   cout << "Array before Sorting: ";
+   display(arr, n);
+   insertionSort(arr, n);
+   cout << "Array after Sorting: ";
+   display(arr, n);
+}
