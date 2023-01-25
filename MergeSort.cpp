9abba426b59bc1e2f48804a5a0418493f28b992c
Suhana0921 <73785663+Suhana0921@users.noreply.github.com>
2023-01-25 23:29:53 +0530

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void swapping(int &a, int &b) {     //swap the content of a and b
+void swapping(int &a, int &b) {   
    int temp;
    temp = a;
    a = b;
@@ -13,16 +13,16 @@ void display(int *array, int size) {
 }
 void merge(int *array, int l, int m, int r) {
    int i, j, k, nl, nr;
-   //size of left and right sub-arrays
+
    nl = m-l+1; nr = r-m;
    int larr[nl], rarr[nr];
-   //fill left and right sub-arrays
+  
    for(i = 0; i<nl; i++)
       larr[i] = array[l+i];
    for(j = 0; j<nr; j++)
       rarr[j] = array[m+1+j];
    i = 0; j = 0; k = l;
-   //marge temp arrays to real array
+   
    while(i < nl && j<nr) {
       if(larr[i] <= rarr[j]) {
          array[k] = larr[i];
@@ -33,11 +33,11 @@ void merge(int *array, int l, int m, int r) {
       }
       k++;
    }
-   while(i<nl) {       //extra element in left array
+   while(i<nl) {       
       array[k] = larr[i];
       i++; k++;
    }
-   while(j<nr) {     //extra element in right array
+   while(j<nr) {     
       array[k] = rarr[j];
       j++; k++;
    }
@@ -46,7 +46,7 @@ void mergeSort(int *array, int l, int r) {
    int m;
    if(l < r) {
       int m = l+(r-l)/2;
-      // Sort first and second arrays
+
       mergeSort(array, l, m);
       mergeSort(array, m+1, r);
       merge(array, l, m, r);
@@ -56,14 +56,14 @@ int main() {
    int n;
    cout << "Enter the number of elements: ";
    cin >> n;
-   int arr[n];     //create an array with given number of elements
+   int arr[n];    
    cout << "Enter elements:" << endl;
    for(int i = 0; i<n; i++) {
       cin >> arr[i];
    }
    cout << "Array before Sorting: ";
    display(arr, n);
-   mergeSort(arr, 0, n-1);     //(n-1) for last index
+   mergeSort(arr, 0, n-1);    
    cout << "Array after Sorting: ";
    display(arr, n);
 }
